2051aae14889a67af5018112a2200a8af86a373b
Marco Giugliani <marco.giugliani@studio.unibo.it>
2023-11-13 11:09:41 +0100

    Done exercise 81

--- 81-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 81-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -21,31 +21,53 @@ public class TestStrictBankAccount {
 
     @BeforeEach
     public void setUp() {
-        fail();
+        this.mRossi = new AccountHolder("Mario", "Rossi", 1);
+        this.bankAccount = new StrictBankAccount(mRossi, 0.0);
     }
 
     // 2. Test the initial state of the StrictBankAccount
     @Test
     public void testInitialization() {
-        fail();
+        Assertions.assertEquals(0.0, bankAccount.getBalance());
+        Assertions.assertEquals(0, bankAccount.getTransactionsCount());
+        Assertions.assertEquals(mRossi, bankAccount.getAccountHolder());
     }
 
 
     // 3. Perform a deposit of 100€, compute the management fees, and check that the balance is correctly reduced.
     @Test
     public void testManagementFees() {
-        fail();
+        double expectedValue = 93.9;
+        Assertions.assertFalse(bankAccount.getTransactionsCount() > 0);
+        this.bankAccount.depositFromATM(1, INITIAL_AMOUNT);
+        Assertions.assertEquals(INITIAL_AMOUNT - ATM_TRANSACTION_FEE, this.bankAccount.getBalance());
+        Assertions.assertTrue(bankAccount.getTransactionsCount() > 0);
+        this.bankAccount.chargeManagementFees(1);
+        Assertions.assertEquals(expectedValue, this.bankAccount.getBalance());
+        Assertions.assertTrue(bankAccount.getTransactionsCount() == 0);
     }
 
     // 4. Test the withdraw of a negative value
     @Test
     public void testNegativeWithdraw() {
-        fail();
+        int negativeNumberToWithdraw = -2;
+        try {
+            this.bankAccount.withdrawFromATM(1, negativeNumberToWithdraw);
+            Assertions.fail();
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals("Cannot withdraw a negative amount", e.getMessage());
+        }
     }
 
     // 5. Test withdrawing more money than it is in the account
     @Test
     public void testWithdrawingTooMuch() {
-        fail();
+        double numberToWithdraw = this.bankAccount.getBalance();
+        try {
+            this.bankAccount.withdrawFromATM(1, numberToWithdraw);
+            Assertions.fail();
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals("Insufficient balance", e.getMessage());
+        }
     }
 }
